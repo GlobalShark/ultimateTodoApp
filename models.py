a9ebc7b3efa6d77db8584ddc308e8660f03822c3
irfanmumtaz <im.irfanmumtaz21@gmail.com>
2018-09-16 03:48:04 +0500

    fixing postgresql issues

--- models.py
+++ models.py
@@ -1,28 +1,26 @@
-import os, sys
 from postgres import db
-from flask import json
-from flask_sqlalchemy import SQLAlchemy
-# from app import app
+from sqlalchemy.dialects.postgresql import JSON
+
 
 class UltimateTodo(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
-    id = db.Column(db.Integer,primary_key=True)
-    email = db.Column(db.String())
-    password = db.Column(db.String())
+    id = db.Column(db.Integer, primary_key=True)
     userName = db.Column(db.String())
+    password = db.Column(db.String())
+    email = db.Column(db.String())
+    gender = db.Column(db.String())
     age = db.Column(db.String())
-    gender= db.Column(db.String())
     joiningdate = db.Column(db.String())
 
-    def __init__(self, id, email, password, userName, age, gender, joiningdate):
-        self.email = email
-        self.password = password
-        self.id = id
+    def __init__(self, userName, password, email, gender, age, joiningdate):
         self.userName = userName
-        self.age = age
+        self.password = password
+        self.email = email
         self.gender = gender
+        self.age = age
         self.joiningdate = joiningdate
 
     def __repr__(self):
-        return '<id {}>'.format(self.id)
+        return "<id {}>".format(self.id)
+
--- postgres.py
+++ postgres.py
@@ -1,60 +1,64 @@
 import os
-from flask import Flask, render_template, url_for, request, send_from_directory, session, jsonify
+from flask import Flask, request, send_from_directory, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask import Session
-import models
 
-app = Flask(__name__, static_folder="./dist", template_folder="./")
+app = Flask(__name__)
 
-app.config.from_object(__name__)
 
 # Postgresql Config
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'ppostgres://rigxhjyv:uoO9zcWIPVanrB0F_YWbjKUwfNKqLFOH@pellefant.db.elephantsql.com:5432/rigxhjyv'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = b'dE\xad2g\x0c\x8d\xb9\x1cq\x86\x04:\xa8>\xc7\xc5\xc2Dr\xe7f\xf9\xeb'
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgres://rigxhjyv:uoO9zcWIPVanrB0F_YWbjKUwfNKqLFOH@pellefant.db.elephantsql.com:5432/rigxhjyv"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config[
+    "SECRET_KEY"
+] = b"dE\xad2g\x0c\x8d\xb9\x1cq\x86\x04:\xa8>\xc7\xc5\xc2Dr\xe7f\xf9\xeb"
 db = SQLAlchemy(app)
 
-
-@app.route('/')
-def index():
-    return render_template("index.html")
+import models
 
 # Register User
-@app.route('/registeruser', methods=['GET', 'POST'])
+@app.route("/registeruser", methods=["GET", "POST"])
 def registerUser():
     data = request.get_json(silent=True)
-    userData = data["user"]
-    print('data', userData['email'])
-    email=userData['email']
-    password=userData['password']
-    userName=userData['userName']
-    age=userData['age']
-    gender=userData['gender']
-    joiningdate=userData['joiningdate']    
+    userData = data
+    print("data", userData["email"])
+    email = userData["email"]
+    password = userData["password"]
+    userName = userData["userName"]
+    age = userData["age"]
+    gender = userData["gender"]
+    joiningdate = userData["joiningdate"]
     userRegister = models.UltimateTodo(
-        1,
-        email,
-        password,
-        userName,
-        age,
-        gender,
-        joiningdate
+        email, password, userName, age, gender, joiningdate
     )
     db.session.add(userRegister)
     db.session.commit()
-    return 'success'
+    return "success"
 
-@app.route('/loginuser', methods=['POST'])
-def loginUser():
-    data = request.get_json(silent=True)
-    print("my data",data)
 
-# If Route not found so redirect to index file
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return render_template("index.html")
+@app.route("/get_users")
+def get_users():
+    _tasks = models.UltimateTodo.query.all()
+    tasks = []
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5050)
+    # iterate tasks
+    for task in _tasks:
+
+        # append data in list
+        tasks.append(
+            {
+                "id": task.id,
+                "userName": task.userName,
+                "password": task.password,
+                "email": task.email,
+                "age": task.age,
+            }
+        )
+    return jsonify({"result": tasks}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5050)
+
