83b6070fe0a0440a55ed5ba1032d311e2dc718b5
Abdul Saboor <asaboor94@gmail.com>
2018-09-25 19:30:17 +0500

    gRPC server woth PostgreSQL

--- grpc-postgresql/server.js
+++ grpc-postgresql/server.js
@@ -1,15 +1,14 @@
-const grpc = require('grpc');
-const protoTodos = grpc.load('todo.proto');
-const server = new grpc.Server();
-const { Client } = require('pg')
-var no_of_items = 0;
-var todos = [{
+const grpc = require('grpc');                // code for using gRPC
+const protoTodos = grpc.load('todo.proto');  // code for connecting protofile in server code
+const server = new grpc.Server();            //code for generating gRPC server
+const { Client } = require('pg')             //code for connecting postgreSQL to server
+
+// list for inserting all tasks(initially empty)                                    
+var todos = [{                               
    
     
 }];
-// Postgress connection
-// const connectionString = 'postgres://postgres:123@localhost/ultimateTodo'
-const connectionString = 'postgres://rumtkkwz:dpiYT-1VQW5rIqPertWCfDgg-s9_Smv1@elmer.db.elephantsql.com:5432/rumtkkwz'
+const connectionString = 'postgres://rumtkkwz:dpiYT-1VQW5rIqPertWCfDgg-s9_Smv1@elmer.db.elephantsql.com:5432/rumtkkwz'   //PostgreSQL url for current server db
 const db = new Client({
     connectionString: connectionString,
 })
@@ -18,19 +17,19 @@ db.connect((err) => {
     console.log('postgres connected')
 
 })
-
+//function for connecting protofile from server
 server.addService(protoTodos.todoproto.TodoService.service, {
-
+// function for getting all tasks from db
     list: function(_, callback) {
           db.query('SELECT * FROM ultimate_todo', (err, res) => {
              if (err) throw err;
               console.log("Tasks in DB are given below: ")
               console.log(res.rows);
+              callback(null,{todos:res.rows})
           })
-  
      },      
 
-
+// function for getting selected task from db
   get: function(call, callback) {
 
  db.query('SELECT * FROM ultimate_todo WHERE todo = $1',[call.request.todo], (err, res) => {
@@ -39,7 +38,7 @@ server.addService(protoTodos.todoproto.TodoService.service, {
               console.log(res.rows);
  })
   }, 
-
+// function for inserting a task into db
  insert: function(call, callback) {
        console.log("my dat",call.request)
        db.query('INSERT INTO ultimate_todo (todo, description) VALUES ($1, $2)', [call.request.todo, call.request.description], (err) => {
@@ -47,12 +46,19 @@ server.addService(protoTodos.todoproto.TodoService.service, {
             console.log("1 document inserted");
        })
  },
-
+// function for deleting requested task from db
        delete: function(call, callback) {
             db.query('DELETE FROM ultimate_todo WHERE todo = $1', [call.request.todo], (err) => {
             if (err) throw err;
              console.log("1 document deleted");
             })
+       },
+// function for updating requested task
+       update: function(call, callback) {
+           db.query('UPDATE ultimate_todo SET description =$1  WHERE todo = $2', [call.request.description, call.request.todo], (err) => {
+            if (err) throw err;
+            console.log("1 document updated");
+           })
        },
  });
  // gRPC Server
