a0174a39e56c604072888f5e3543ec64b7d4a913
babar7 <babarshair97@gmail.com>
2018-09-30 21:49:06 +0500

    NavBar

---
+++ Part3/src/components/AppBar.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import history from "../History";
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title" color="inherit" className={classes.grow}>
+            Todo List App
+          </Typography>
+          {(localStorage.getItem("currentUserUid") === "")? 
+          <div>
+          <Button color="inherit" onClick={ () => { history.push('/signup')}}>Signup</Button>
+          <Button color="inherit" onClick={ () => { history.push('/signin')}}>Signin</Button>
+          </div>
+        :
+        <Button color="inherit" onClick={ () => {localStorage.removeItem("currentUserUid");  history.push('/signin')}}>Logout</Button>
+        }
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
