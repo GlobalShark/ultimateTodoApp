3ecb3a5453d85822a59e20bc2ebd4ca3bf12a3bc
babar7 <babarshair97@gmail.com>
2018-09-30 21:50:31 +0500

    Firebase Config

---
+++ Part3/src/config/firebaseConfig.js
@@ -0,0 +1,13 @@
+ 
+ // Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyAmD9GHv441vUXwH3H1Hr_mM-BA6vihwPk",
+  authDomain: "myultimatetodolistapp.firebaseapp.com",
+  databaseURL: "https://myultimatetodolistapp.firebaseio.com",
+  projectId: "myultimatetodolistapp",
+  storageBucket: "myultimatetodolistapp.appspot.com",
+  messagingSenderId: "516578628187"
+};
+// firebase.initializeApp(config);
+
+export default config;
